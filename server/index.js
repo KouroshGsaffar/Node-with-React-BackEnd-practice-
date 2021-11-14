1523e1dde39aab6e6dc04b88939b75bf5afd83bd
KouroshGSaffar <kooroshgharooni74@gmail.com>
2021-11-13 22:38:19 -0500

    created the mongoDB and save userId in db

--- server/index.js
+++ server/index.js
@@ -1,22 +1,14 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
 
-const keys=require('./config/keys')
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret:keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken)=>{
-    console.log(accessToken);
-}));
+require('./routes/authRoutes')(app);
 
-app.get('/auth/google', passport.authenticate('google', {
-    scope:['profile', 'email'],
-}));
-app.get('/auth/goolge/callback', passport.authenticate('google'));
-
-const PORT=process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT);
